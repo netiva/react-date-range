8ea90776c79ad396f376c97a13623c4658d6a005
Azra Vinko Novak <azra.vinko.novak@signet.hr>
2019-09-23 13:01:11 +0200

    Revert changes

--- src/components/Month.js
+++ src/components/Month.js
@@ -26,7 +26,7 @@ function renderWeekdays(styles, dateOptions) {
         end: endOfWeek(now, dateOptions),
       }).map((day, i) => (
         <span className={styles.weekDay} key={i}>
-          {format(day, 'EEEEE', dateOptions)}
+          {format(day, 'ddd', dateOptions)}
         </span>
       ))}
     </div>
